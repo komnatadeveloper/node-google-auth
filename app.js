30ad8f203362941c934b354b0e20bdcb0b0b988d
Emrah <ozyazgane@hotmail.com>
2020-10-18 21:06:45 +0300

    Initial things before Passport.js implementation

---
+++ app.js
@@ -0,0 +1,55 @@
+const express = require ('express');
+const path = require('path');
+const dotenv = require('dotenv');
+
+const morgan = require('morgan');
+const exphbs = require('express-handlebars');
+const connectDB = require('./config/db');
+
+
+// Load config
+dotenv.config({
+  path: './config/config.env'
+})
+
+// Connect to MongoDB
+connectDB();
+
+const app = express();
+
+if ( process.env.NODE_ENV === 'development' ) {
+  app.use(morgan('dev'))
+}  
+
+// Handlebars
+app.engine(
+  '.hbs', 
+  exphbs(
+    { 
+      defaultLayout: 'main',
+      extname: '.hbs' 
+    }
+  )
+);
+
+// Static Folder
+app.use(
+  express.static(path.join(
+    __dirname, 'public'
+  ))  
+)
+
+
+// Routes
+app.use('/', require('./routes/index'))
+
+app.set('view engine', '.hbs');
+
+const PORT = process.env.PORT || 3000
+
+app.listen( 
+  PORT, 
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port: ${PORT}`
+  )
+);
